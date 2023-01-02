13b93e3517ee7477f1e0f8c3e71b9b179c5560cc
dong1hyun <limd1238@naver.com>
2023-01-02 16:44:52 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [USD, setUSD] = useState(0);
+  const onChange = (event) => setUSD(event.target.value);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then(res => res.json()) // JSON 응답을 JavaScript 객체 리터럴로 구문분석합니다.
+      .then(json => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? <strong>Loading...</strong> : (
+        <div>
+          <select>
+            {coins.map((coin) => (
+              <option key={coin.id}>
+                {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+              </option>
+            ))};
+          </select>
+          <h3>How much USD do you have</h3>
+          <input onChange={onChange} placeholder="USD" />
+          <h2>You can buy {Math.round(USD / coins[0].quotes.USD.price)} BTC</h2>
+        </div>
+      )}
     </div>
   );
 }
